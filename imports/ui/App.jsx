d9157820c378e8d60ceb607ee62aea383e2598ee
Brian Duffey <briand@mrc-productivity.com>
2018-04-25 19:08:13 -0500

    Added immutable js for state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,39 @@
 'use strict';
 
 import React, { Component } from 'react';
+import { List, Map } from 'immutable';
 
+import Filters from './Filters';
 import Header from './Header';
 import MovieItemsContainer from './MovieItemsContainer';
+import Toolbar from './Toolbar';
 
 class App extends Component {
 	constructor (props) {
 		super(props);
+
+		this.state = {
+			sortBy: List(['ITEMID', 'ASC']),
+			filterOpen: false,
+			filters: Map()
+		};
+
+		this._toggleFilters = this._toggleFilters.bind(this);
+	}
+
+	_toggleFilters () {
+		this.setState(prevState => ({ filterOpen: !prevState.filterOpen }));
 	}
 
 	render () {
+		const { filterOpen, filters, sortBy } = this.state;
+
 		return (
 			<div className="full-coverage">
 				<Header />
-				<MovieItemsContainer />
+				<Toolbar sortBy={sortBy} toggleFilters={this._toggleFilters} />
+				{filterOpen ? <Filters filters={filters} /> : null}
+				<MovieItemsContainer filters={filters} sortBy={sortBy} />
 			</div>
 		);
 	}
