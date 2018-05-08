e1b549d2afc57b2593f7ec28799ec88a381d0d91
Brian Duffey <briand@mrc-productivity.com>
2018-05-08 12:07:27 -0500

    Removed dependency on bulma-scss-version for my own package

--- imports/ui/MovieItemGridContainer.jsx
+++ imports/ui/MovieItemGridContainer.jsx
@@ -4,7 +4,6 @@ import { Container } from 'bloomer';
 import styled from 'styled-components';
 
 const MovieItemGridContainer = styled(Container)`
-	box-sizing: border-box;
 	height: calc(100vh - 52px - 52px);
 	overflow-y: auto;
 	padding: 1px 13px;
