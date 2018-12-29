d7d2cd20ebfdaadcfed65d82b45e69bb047bcf0b
bduff9 <bduff9@gmail.com>
2018-12-29 13:48:17 -0600

    Updated apollo

--- client/main.js
+++ client/main.js
@@ -1,15 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import React from 'react';
-import ApolloClient from 'apollo-client';
-// @ts-ignore
-import { createApolloClient } from 'meteor/apollo';
 import { ApolloProvider } from 'react-apollo';
 
 import App from '../imports/ui/App/App.jsx';
+import client from '../imports/startup/client/apollo';
 import '../imports/startup/client';
-
-const client = new ApolloClient(createApolloClient());
 
 Meteor.startup(() => {
 	render(<React.StrictMode>
--- imports/startup/server/apollo.js
+++ imports/startup/server/apollo.js
@@ -1,16 +1,23 @@
+import { ApolloServer } from 'apollo-server-express';
 // @ts-ignore
-import { createApolloServer } from 'meteor/apollo';
-import { makeExecutableSchema } from 'graphql-tools';
-import typeDefs from '../../api/schema';
+import { getUser } from 'meteor/apollo';
+import { WebApp } from 'meteor/webapp';
+import typeDefs from '../../api/schema.graphql';
 import resolvers from '../../api/resolvers';
 
-const schema = makeExecutableSchema({
+const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	allowUndefinedInResolve: false,
-	logger: { log: err => console.error('GraphQL Custom Logger', err) },
+	context: async ({ req }) => ({
+		user: await getUser(req.headers.authorization),
+	}),
 });
 
-createApolloServer({
-	schema,
+server.applyMiddleware({
+	app: WebApp.connectHandlers,
+	path: '/graphql',
+});
+
+WebApp.connectHandlers.use('/graphql', (req, res) => {
+	if (req.method === 'GET') res.end();
 });
